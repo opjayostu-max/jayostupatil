61a6b07569a93f7420d35aeb9749be2d5ce96aae
opjayostu-max <opjayostu@gmail.com>
2026-02-05 20:44:37 +0530

    Delete theme.js

--- theme.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector(".theme-btn");
-const icon = btn.querySelector("i");
-
-btn.onclick = () => {
-  document.body.classList.toggle("light");
-  icon.className = document.body.classList.contains("light")
-    ? "fa-solid fa-sun"
-    : "fa-solid fa-moon";
-};
